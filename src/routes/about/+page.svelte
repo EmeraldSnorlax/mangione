208814da7b88e6cf42f2a0be8aa664f18d0900a4
Rain <emeraldsnorlax@disroot.org>
2025-01-12 18:30:04 +0000

    edited draft?

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-  <title>About</title>
+	<title>About</title>
 </svelte:head>
 
 <a href="/" class="mt-8 block text-xl lg:mt-36">Back</a>
@@ -25,16 +25,41 @@
 	comments under the above video.
 </p>
 
+<p>It's an open secret that social media platforms exist on outrage and controversy.</p>
+
+<p>
+	But this was more significant; more unusual, than run-of-the-mill social media trolling. The
+	aftermath of the assassination of Brian Thompson has been a time when it became common (and even
+	socially popular) to openly and loudly disagree with the ethics we've been taught about something
+	as seemingly universally shunned as murder. People on social media from both sides of the
+	political aisle felt a sense of apathy or schadenfreude.
+</p>
+
 <p>
 	This has been one of the few times when a large amount of conservative Americans have rejected
 	right-wing media and shown anything approaching class consciousness.
 </p>
 
 <p>
-	Many people on social media from both sides of the political aisle felt a sense of schadenfreude
-	about the murder.
+	The ruling class have long been class conscious. Their ownership of most mainstream news was
+	swiftly put to use: Brian Thompson was not CEO of a corporation that was the posterchild of
+	everything wrong with the US healthcare industry; he was <a
+		href="https://web.archive.org/web/20250103201545/https://www.nytimes.com/2024/12/10/nyregion/unitedhealthcare-brian-thompson-funeral.html"
+		>'a loving husband' with a 'stunned family'.</a
+	>
 </p>
 
+<p>He came from 'modest roots.'</p>
+
+<p>He 'ascended to one of the most powerful roles in the healthcare industry.'</p>
+
+<p>
+	Humanising someone indirectly responsible for mass systemic violence was <a
+		href="https://twitter.com/esjesjesj/status/1864794797581455594"
+		>predictably not well recieved, especially when published by outlets known for vilifying
+		working-class criminals and victims alike.</a
+	>
+</p>
 <p>
 	<a
 		href="https://www.politico.com/newsletters/politico-nightly/2024/12/12/the-political-lessons-of-luigi-mangiones-media-diet-00194163"
@@ -43,23 +68,43 @@
 </p>
 
 <p>
-	You could conveniently filter out parts that you didn't agree with, and project your own morality
+	You could filter out parts that you didn't agree with, and project your own morality
 	onto Mangione. In most cases, this was already done for you by the publications and people you
 	follow.
 </p>
 
-<p>The aftermath of the assassination of Brian Thompson has been a time when it became common (and even socially popular) to openly and loudly disagree with the ethics we've been taught about something as seemingly universally shunned as murder.</p>
+<p>
+	To those who feared class consciousness, Mangione is a domestic terrorist. <a
+		href="https://www.kenklippenstein.com/p/new-crisis-hotline-for-ceos"
+		>Worthy of considering a 'threatened CEO hotline' for.</a
+	> The media was a tool to achieve this goal.
+</p>
 
 <p>
 	Our media diets have an immense impact on our view of the world. We live in a highly polarised
 	political climate, where how much something resonates with you is often more important than how
-	true it is.
+	true it is. The ruling class have known this for centuries.
 </p>
 
-<hr>
+<p>
+	Mangione is <a
+		href="https://www.theguardian.com/commentisfree/2024/dec/13/luigi-mangione-american-voter"
+		>eerily similar politically to the average voter
+	</a>
+	-- focused on issues pushed by the media, rather than coherent, systemic action.
+</p>
+
+<p>
+	If this is the face of the first sparks of modern mainstream class consciousness in America,
+	we're fucked.
+</p>
+
+<hr />
 
 <p class="pb-32">
-	This piece is a literal filtering of Mangione's manifesto through the publications <strong>you</strong> have read, courtesy of your browser history.
+	This piece is a literal filtering of Mangione's manifesto through the publications <strong
+		>you</strong
+	> have read, courtesy of your browser history.
 </p>
 
 <style>
