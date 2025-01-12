1495d64c01a16f7ac7384a22dfb2c4acc5bf5b99
Rain <emeraldsnorlax@disroot.org>
2025-01-12 19:19:45 +0000

    minor grammar edits and tightening of language

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,43 +7,43 @@
 <h1>About</h1>
 <p>On the 4th December 2024, Brian Thompson, the CEO of UnitedHealth, was shot.</p>
 <p>
-	The internet's reaction to this was chaotic. Misinformation rapidly spread as people <a
+	The internet's reaction to this was chaotic. Misinformation spread rapidly as people <a
 		href="https://time.com/7201952/luigi-mangione-internet-myths-essay/"
-		>cherry-picked pieces of Luigi Mangione</a
-	> to try and make sense of him; a person with motives that seemingly didn't fit into any coherent ideology.
+		>cherry-picked pieces of Luigi Mangione, the shooter,</a
+	> to try and make sense of him; a person with motives that fit into no coherent ideology.
 </p>
 
 <p>
-	Media publications and influencers, particularly right-wing leaning ones, were quick to condemn
-	the murder. Ben Shapiro, for example, was quick to ascribe tolerance of it as being an <a
+	Media publications and influencers, particularly those on the political right-wing, were quick to
+	condemn the murder. Ben Shapiro, for example, was quick to ascribe tolerance of it as being an <a
 		href="https://www.youtube.com/watch?v=GeRnWYn-GTQ">'evil leftist revolutionary'.</a
 	>
 </p>
 
 <p>
-	However, readers and viewers of those publications responded poorly, as can be seen with the
-	comments under the above video.
+	However, readers and viewers of these publications responded poorly, as can be seen when reading
+	the comments under the above video.
 </p>
 
 <p>It's an open secret that social media platforms exist on outrage and controversy.</p>
 
 <p>
-	But this was more significant; more unusual, than run-of-the-mill social media trolling. The
-	aftermath of the assassination of Brian Thompson has been a time when it became common (and even
-	socially popular) to openly and loudly disagree with the ethics we've been taught about something
-	as seemingly universally shunned as murder. People on social media from both sides of the
-	political aisle felt a sense of apathy or schadenfreude.
+	But this was more significant; more unusual, than run-of-the-mill social media trolling. In the
+	aftermath Brian Thompson's assassination, it became common (and even socially popular) to openly
+	and loudly disagree with the ethics we've been taught, even about something as seemingly
+	universally shunned as murder. Social media users from across the political aisle felt a sense of
+	apathy or schadenfreude.
 </p>
 
 <p>
-	This has been one of the few times when a large amount of conservative Americans have rejected
+	This has been one of the few times when a large number of conservative Americans have rejected
 	right-wing media and shown anything approaching class consciousness.
 </p>
 
 <p>
 	The ruling class have long been class conscious. Their ownership of most mainstream news was
-	swiftly put to use: Brian Thompson was not CEO of a corporation that was the posterchild of
-	everything wrong with the US healthcare industry; he was <a
+	swiftly put to use: Brian Thompson was not CEO of the posterchild of everything wrong with the US
+	healthcare industry; he was <a
 		href="https://web.archive.org/web/20250103201545/https://www.nytimes.com/2024/12/10/nyregion/unitedhealthcare-brian-thompson-funeral.html"
 		>'a loving husband' with a 'stunned family'.</a
 	>
@@ -56,7 +56,7 @@
 <p>
 	Humanising someone indirectly responsible for mass systemic violence was <a
 		href="https://twitter.com/esjesjesj/status/1864794797581455594"
-		>predictably not well recieved, especially when published by outlets known for vilifying
+		>predictably poorly recieved, especially when published by outlets known for vilifying
 		working-class criminals and victims alike.</a
 	>
 </p>
@@ -68,13 +68,12 @@
 </p>
 
 <p>
-	You could filter out parts that you didn't agree with, and project your own morality
-	onto Mangione. In most cases, this was already done for you by the publications and people you
-	follow.
+	You could filter out the parts that you didn't agree with, and project your own morality onto
+	him. In most cases, this was already done for you by the publications and people you follow.
 </p>
 
 <p>
-	To those who feared class consciousness, Mangione is a domestic terrorist. <a
+	To those who fear class consciousness, Mangione is a domestic terrorist. <a
 		href="https://www.kenklippenstein.com/p/new-crisis-hotline-for-ceos"
 		>Worthy of considering a 'threatened CEO hotline' for.</a
 	> The media was a tool to push this narrative.
@@ -95,8 +94,8 @@
 </p>
 
 <p>
-	If this is the face of the first sparks of modern mainstream class consciousness in America,
-	we're fucked.
+	If this is the face of the first sparks of modern mainstream class consciousness in America, we're
+	fucked.
 </p>
 
 <hr />
